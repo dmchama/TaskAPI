ba690c8bc1cc0351524073a2130ff60065e9fcfd
chamaracz <chamara@codezync.com>
2025-08-27 23:12:13 +0530

    add models and services

---
+++ TaskAPI/Services/TodoService.cs
@@ -0,0 +1,40 @@
+﻿using TaskAPI.Models;
+
+namespace TaskAPI.Services
+{
+    public class TodoService
+    {
+        public List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "title 1",
+                Description = "des 1",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+
+            };
+
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "title 2",
+                Description = "des ",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(4),
+                Status = TodoStatus.New
+
+            };
+
+            todos.Add(todo2);
+
+            return todos;
+        }
+    }
+}
